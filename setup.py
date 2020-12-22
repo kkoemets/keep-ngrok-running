bb5662bdfd0fe7b6f396946935ad86b05eea8235
Kristjan Koemets <koemets.kristjan@gmail.com>
2020-12-22 14:28:05 +0200

    Add requirements.txt & setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+    Setup file for keep-ngrok-running.
+    Use setup.cfg to configure your project.
+
+    This file was generated with PyScaffold 3.2.3.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
+import sys
+
+from pkg_resources import VersionConflict, require
+from setuptools import setup
+
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+setup(name='keep-ngrok-running',
+      version='0.1',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      license='MIT',
+      install_requires=[
+          'boto3',
+          'requests',
+      ],
+      zip_safe=False)
+
+if __name__ == "__main__":
+    setup(use_pyscaffold=True)
+
